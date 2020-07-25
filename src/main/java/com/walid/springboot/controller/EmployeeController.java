a60a5e2797a23dc9c2e959c824398c4ba7b401bb
wa1id <wa1id@users.noreply.github.com>
2020-07-25 19:05:32 +0200

    Get employee by Id + add ResourceNotFoundException

--- src/main/java/com/walid/springboot/controller/EmployeeController.java
+++ src/main/java/com/walid/springboot/controller/EmployeeController.java
@@ -1,9 +1,12 @@
 package com.walid.springboot.controller;
 
+import com.walid.springboot.exception.ResourceNotFoundException;
 import com.walid.springboot.model.Employee;
 import com.walid.springboot.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +26,14 @@ public class EmployeeController {
     }
 
     //GET employee by id
+    @GetMapping("employees/{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable(value = "id") Long employeeId) throws ResourceNotFoundException {
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not found for this id: " + employeeId));
+
+        return ResponseEntity.ok(employee);
+    }
+
     //POST employee (save, new)
     //PUT employee (update)
     //DELETE employee
